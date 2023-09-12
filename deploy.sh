4fd728b724f481356d0f96f19104265e75061060
xkhm <im.xkhm@gmail.com>
2023-09-13 04:21:28 +0800

    feat: 部署

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,7 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
-git push -f git@github.com:yukiyukixing/interview-blog.git main
+# git push -f git@github.com:yukiyukixing/interview-blog.git main
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
