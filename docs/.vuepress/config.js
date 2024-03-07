c4a94d5c426c2a7febfd77488d9e49f4d166de88
xkhm <im.xkhm@gmail.com>
2024-03-08 01:18:49 +0800

    feat: 添加监控

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,7 +2,18 @@ module.exports = {
     base: '/interview-blog/',
     plugins: ['@vuepress/back-to-top'],
     head: [
-        ['link', { rel: 'icon', href: '/favicon.png' }]
+        ['link', { rel: 'icon', href: '/favicon.png' }],
+        [
+            "script",
+            {},
+            `
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "ld1lb1mxcw");
+            `
+        ]
     ],
     configureWebpack: {
         resolve: {
